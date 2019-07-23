28a76aae715b35083e6b005afbf084b6a9f79978
Asén Ranguelov <asenranguelov@.github.com>
2019-07-24 00:11:28 +0300

    Initial commit - Create the first part of the model

--- tweetNsell-server/tweetNsellServer/API/models.py
+++ tweetNsell-server/tweetNsellServer/API/models.py
@@ -1,4 +1,104 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import JSONField
+
+class ServiceIndustry(models.Model):
+    name_en = models.CharField(max_length = 30)
+    name_es = models.CharField(max_length = 30)
+    
+    def __str__(self):
+        return "%s (%s)" % (self.name_en, self.name_es)
+
+class Administrator(models.Model):
+    user_profile = models.OneToOneField(User, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.user_profile.username
+
+
+class Brand(models.Model):
+    user_profile = models.OneToOneField(User, on_delete = models.CASCADE, primary_key=True)
+    id = models.CharField(max_length = 30)
+    name = models.CharField(max_length = 50, blank = True)
+    location = models.CharField(max_length = 40, blank = True)
+    description = models.TextField(blank = True)
+
+    ENGLISH = 'en'
+    SPANISH = 'es'
+    language_choices = [
+        (ENGLISH, 'English'),
+        (SPANISH, 'Español')
+    ]
+    language = models.CharField(
+        max_length = 2,
+        choices = language_choices,
+        default = ENGLISH
+    )
+
+    url = models.CharField(max_length = 60, blank = True)
+    is_verified = models.BooleanField(default = False)
+    social_rating = JSONField()
+    are_all_opinions_evaluated = models.BooleanField(default = True)
+
+    service_industry = models.ForeignKey(ServiceIndustry, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.user_profile.username
+
+
+class Opinion(models.Model):
+    id = models.CharField(max_length = 30, primary_key=True)
+    text = models.TextField(blank = True)
+
+    ENGLISH = 'en'
+    SPANISH = 'es'
+    language_choices = [
+        (ENGLISH, 'English'),
+        (SPANISH, 'Español')
+    ]
+    language = models.CharField(
+        max_length = 2,
+        choices = language_choices,
+        default = ENGLISH
+    )
+
+    publication_moment = models.DateTimeField()
+    number_favorites = models.PositiveIntegerField()
+    number_retweets = models.PositiveIntegerField()
+    is_latest = models.BooleanField(default = False)
+    is_pinned = models.BooleanField(default = False)
+
+    POSITIVE = 'pos'
+    NEGATIVE = 'nwg'
+    NEUTRAL = 'neu'
+    UNCATEGORIZED = 'unc'
+
+    attitude_choices = [
+        (POSITIVE, 'Positive'),
+        (NEGATIVE, 'Negative'),
+        (NEUTRAL, 'Neutral'),
+        (UNCATEGORIZED, 'Uncategorized'),
+    ]
+    attitude = models.CharField(
+        max_length = 3,
+        choices = attitude_choices,
+        default = UNCATEGORIZED
+    )
+
+
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.id
+
+
+
+
+
+
+
+
+
 
 
 

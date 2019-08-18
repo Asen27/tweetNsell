1cd896d58ebc50d1c702297b3c6d54c39a384d36
Asén Ranguelov <asenranguelov@.github.com>
2019-08-18 19:56:42 +0300

    Add STATIC_ROOT variable in settings.py

--- tweetNsell-server/tweetNsellServer/settings.py
+++ tweetNsell-server/tweetNsellServer/settings.py
@@ -138,6 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':
@@ -151,5 +152,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+
 import django_heroku
 django_heroku.settings(locals())
+
+

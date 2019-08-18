06dab7118e0283cf5da33d76283968f387b525ea
Asén Ranguelov <asenranguelov@.github.com>
2019-08-18 20:17:23 +0300

    Remove STATIC_ROOT variable from settings.py

--- tweetNsell-server/tweetNsellServer/settings.py
+++ tweetNsell-server/tweetNsellServer/settings.py
@@ -137,8 +137,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':

6544eb06d90cbe673d8882a22558061f46d37d8d
Asén Ranguelov <asenranguelov@.github.com>
2019-08-19 10:32:42 +0300

    Deploy to Heroku (6)

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/tweet-n-sell'));
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
+app.use(allowCrossDomain);
+
 app.get('/*', function(req,res) {
 
 res.sendFile(path.join(__dirname+'/dist/tweet-n-sell/index.html'));
